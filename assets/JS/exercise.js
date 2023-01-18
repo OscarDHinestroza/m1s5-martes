b016b1f37621d3d5bc1ed48ee0cfd82631d60847
Oscar D Hinestroza <danielkdmorales@hotmail.com>
2023-01-17 22:32:57 -0500

    corte de clase martes 17 ene

---
+++ assets/JS/exercise.js
@@ -0,0 +1,11 @@
+let carName = "Volvo"
+let x = 50;
+let a = 5;
+let b = 10;
+
+let suma = a + b;
+
+console.log("carName", carName);
+console.log("La suma es:", suma);
+
+console.log("suma:", a+b);
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -10,3 +10,23 @@ const fechaNacimiento = "25/01/1998";
 let apellido = "Hinestroza";
 
 console.log(apellido);
+
+let nombreCompleto = "Oscar Hinestroza" //string (text)
+let telefono = 123456789; // number (número)
+let humano = true; // boolean (true/falso)
+let direccion; //undefined (no value)
+let codigoPostal = null; //null
+
+/*
+console.log("nombreCompleto", typeof nombreCompleto);
+console.log("telefono", typeof telefono);
+console.log("humano", typeof humano);
+console.log("direccion", typeof direccion);
+console.log("codigoPostal", typeof codigoPostal);
+*/
+
+console.log("nombreCompleto", nombreCompleto);
+console.log("telefono", telefono);
+console.log("humano", humano);
+console.log("direccion", direccion);
+console.log("codigoPostal", codigoPostal);
