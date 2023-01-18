10dafd6d9dd77a19a6810f4743f41d70e52be151
Oscar D Hinestroza <danielkdmorales@hotmail.com>
2023-01-17 22:35:53 -0500

    Updated exercise.js

--- assets/JS/exercise.js
+++ assets/JS/exercise.js
@@ -1,11 +1,17 @@
 let carName = "Volvo"
+let driver = "Oscar"
 let x = 50;
 let a = 5;
 let b = 10;
 
-let suma = a + b;
+// let suma = a + b;
+let suma = Number (a) + Number (b);
+
+let concatenacion = carName + driver;
 
 console.log("carName", carName);
 console.log("La suma es:", suma);
 
 console.log("suma:", a+b);
+
+console.log("La concatenación es:", concatenacion);
